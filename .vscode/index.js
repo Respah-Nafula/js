8e9b664d9f7a702a7d4978ace918877973b47519
Respah-Nafula <nafularespah@gmail.com>
2022-05-09 11:16:42 +0300

    first commit

---
+++ .vscode/index.js
@@ -0,0 +1,24 @@
+//This is my first Javascript code!
+console.log('Hello world');
+
+   let name = "Respah";
+   let age = 30;//Number literal
+   let isApproved = true;
+   let FirstName = undefined;
+   let lastName = null;
+   console.log(name);
+   let person = {
+       name: "Respah"
+    //Dot notation
+
+   };
+   // variables cannot be key words
+//names should be meaningful and descriptive
+//variables cannot start with a number
+//variables cannot contain a space or hyphen
+//variables are case sensitive
+let firstName = "Nafula";
+lastName = "Phanuel"
+let interestRate = 0.3;
+interRate = 1;
+//
